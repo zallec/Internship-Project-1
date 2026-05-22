0ea5fa73158e39cd62fca27f112f43385b71d7ee
zallec <zacharyallec@gmail.com>
2026-05-22 08:23:33 -0700

    improving mechanics

--- scenes/scripts/ball.gd
+++ scenes/scripts/ball.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	position = Vector2(0,0)
-	var direction: float = position.angle_to_point(randf_range(PI, -PI))
+	var direction: float = position.angle_to_point(randf_range(-PI, PI))
 	var velocity: Vector2 = Vector2(randf_range(150.0, 200.0), 0.0)
 	ball.linear_velocity = velocity.rotated(direction)
 
