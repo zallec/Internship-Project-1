ec6c0df9d40ca1cf83b7bad770dde4160f90236f
zallec <zacharyallec@gmail.com>
2026-05-22 00:02:29 -0700

    player/enemy wall logic

--- scenes/scripts/enemyWall.gd
+++ scenes/scripts/enemyWall.gd
@@ -1,12 +1,35 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 # !!! Needs movement clamped between top and bottom of screen
+@export var speed: float = 400 #export allows editing this variable from the inspector
+var screen_height: float
+var ball: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	screen_height = get_viewport_rect().size.y
+	ball = get_node("res://scenes/ball")
+	position = Vector2(-348,0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if ball.position.y > position.y + 10:  # Dead zone prevents jittering
+		velocity.y = speed
+	elif ball.position.y < position.y - 10:
+		velocity.y = -speed
+	else:
+		velocity.y = 0
+
+	move_and_slide()
+	position.y = clamp(position.y, -(screen_height / 2), 298)
+	
+	# if collides with object
+	#for area in get_overlapping_bodies():
+		#if area.is_in_group("ball"):
+			
+		#if area.is_in_group("enemy"):
+			
+
+func start() -> void:
+	show()
--- scenes/scripts/playerWall.gd
+++ scenes/scripts/playerWall.gd
@@ -1,18 +1,39 @@
-extends Area2D
+extends CharacterBody2D
 
 # !!! Needs movement clamped between top and bottom of screen
+@export var speed: float = 400 #export allows editing this variable from the inspector
+var screen_height: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var screen_size: Vector2 = get_viewport_rect().size
+	screen_height = screen_size.y
+	print("screen_height: ", screen_height)
+	print("position: ", position)
+	position = Vector2(-348,0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	var wall_height: int = 40
+	move_and_slide()
+	position.y = clamp(position.y, -(screen_height / 2), 298)
+	
 	# if collides with object
 	#for area in get_overlapping_bodies():
 		#if area.is_in_group("ball"):
 			
 		#if area.is_in_group("enemy"):
 			
+
+func start() -> void:
+	show()
