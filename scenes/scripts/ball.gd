c1b8823d1c1c6b9d80265e77fd273d2aa13165e2
zallec <zacharyallec@gmail.com>
2026-05-23 10:35:10 -0700

    added ball's physics and collision

--- scenes/scripts/ball.gd
+++ scenes/scripts/ball.gd
@@ -1,17 +1,19 @@
-extends RigidBody2D
+extends CharacterBody2D
 
+@export var SPEED: float = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position = Vector2(0,0)
+	var direction: float = randf_range(-PI, PI)
+	velocity = Vector2(SPEED, 0.0).rotated(direction)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
-	position = Vector2(0,0)
-	var direction: float = position.angle_to_point(randf_range(-PI, PI))
-	var velocity: Vector2 = Vector2(randf_range(150.0, 200.0), 0.0)
-	ball.linear_velocity = velocity.rotated(direction)
+func _physics_process(delta: float) -> void:
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
 
 func _start() -> void:
 	pass
