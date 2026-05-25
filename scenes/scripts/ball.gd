ed6cbe28decce51dab448182eecc936a93358896
zallec <zacharyallec@gmail.com>
2026-05-25 16:11:51 -0700

    changed ball spawn and enemy wall logic for consistant ball respawns

--- scenes/scripts/ball.gd
+++ scenes/scripts/ball.gd
@@ -1,19 +1,28 @@
 extends CharacterBody2D
 
-@export var SPEED: float = 200
+@export var speed: float = 300
+@export var max_speed: float = 500
+var direction: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position = Vector2(576,323)
-	var direction: float = randf_range(-PI, PI)
-	velocity = Vector2(SPEED, 0.0).rotated(direction)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
 	if collision:
+		speed = speed + 50
+		direction = velocity.bounce(collision.get_normal()).normalized()
 		velocity = velocity.bounce(collision.get_normal())
+		
 
 func _start() -> void:
-	pass
+	position = Vector2(576,323)
+	speed = 300
+	var angle: float = randf_range(-PI/4, PI/4)
+	if randi() % 2 == 0:
+		angle += PI
+	direction = Vector2(cos(angle), sin(angle))
+	velocity = direction * speed
