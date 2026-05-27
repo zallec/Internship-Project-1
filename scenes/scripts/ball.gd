86c55a1bc957c48e08addb420f282f6436fa9fee
zallec <zacharyallec@gmail.com>
2026-05-27 08:43:59 -0700

    redid ball logic using enum states to fix spawn issues and keep the same ball throughout match. Player & enemy walls are neatened and overall code is smaller but has better performance

--- scenes/scripts/ball.gd
+++ scenes/scripts/ball.gd
@@ -1,30 +1,59 @@
 extends CharacterBody2D
 
-@export var speed: float = 25
-@export var max_speed: float = 500
+@export var speed: float = 15
+@export var max_speed: float = 50
+@export var ball_delay: float = 2
 var direction: Vector2 = Vector2.ZERO
+var starting_pos: Vector2 = Vector2(573, 323)
+var launch_timer: float = 0
 
+enum ball_state {WAITING, LAUNCH, MOVING}
+var cur_state := ball_state.WAITING
+
+signal ball_launch
+signal launch_time
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_physics_process(false)
-	hide()
+	starting_pos = global_position
+	reset()
+
+
+func reset() -> void:
+	cur_state = ball_state.WAITING
+	velocity = Vector2.ZERO
+	global_position = starting_pos
+	launch_timer = ball_delay
+	ball_launch.emit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var collision: KinematicCollision2D = move_and_collide(velocity * speed * delta)
-	# handles bounce physics, supposed to increase velocity when bouncing
-	if collision:
-		direction = velocity.bounce(collision.get_normal()).normalized()
-		speed = speed * 1.01
-		velocity = velocity.bounce(collision.get_normal())
+	match cur_state:
+		ball_state.WAITING:
+			# counts down using delta instead of timer for live updates
+			launch_timer -= delta
+			var time: float = 1.0 - (launch_timer / ball_delay)
+			emit_signal("launch_time", time)
+			if launch_timer <= 0:
+				launch()
+				
+		ball_state.MOVING:
+			var collision: KinematicCollision2D = move_and_collide(velocity * speed * delta)
+			# handles bounce physics, supposed to increase velocity when bouncing
+			if collision:
+				direction = velocity.bounce(collision.get_normal()).normalized()
+				speed = speed * 1.01
+				velocity = velocity.bounce(collision.get_normal())
 
 
-func start() -> void:
-	show()
+func launch() -> void:
+	# launch math
 	var angle: float = randf_range(-PI/6, PI/6)
 	if randi() % 2 == 0:
 		angle += PI
 	direction = Vector2(cos(angle), sin(angle))
 	velocity = direction * speed
+	
+	# change state to moving
+	cur_state = ball_state.MOVING
